e0a1842e5b6ab98465a5e633bca8e2f4b88a02cd
Jack-Keene <Jack-Keene@users.noreply.github.com>
2022-09-15 13:47:25 +0000

    deploy: d33510ba8a3c4f32e6f82e728e6874b63ecbe326

--- js/learn.js
+++ js/learn.js
@@ -48,66 +48,66 @@ function fallbackMessage(action) {
 }
 
 function switchTab(tabGroup, tabId) {
-    allTabItems = jQuery("[data-tab-group='"+tabGroup+"']");
-    targetTabItems = jQuery("[data-tab-group='"+tabGroup+"'][data-tab-item='"+tabId+"']");
+    allTabItems = jQuery("[data-tab-group='" + tabGroup + "']");
+    targetTabItems = jQuery("[data-tab-group='" + tabGroup + "'][data-tab-item='" + tabId + "']");
 
     // if event is undefined then switchTab was called from restoreTabSelection
     // so it's not a button event and we don't need to safe the selction or
     // prevent page jump
     var isButtonEvent = event != undefined;
 
-    if(isButtonEvent){
-      // save button position relative to viewport
-      var yposButton = event.target.getBoundingClientRect().top;
+    if (isButtonEvent) {
+        // save button position relative to viewport
+        var yposButton = event.target.getBoundingClientRect().top;
     }
 
     allTabItems.removeClass("active");
     targetTabItems.addClass("active");
 
-    if(isButtonEvent){
-      // reset screen to the same position relative to clicked button to prevent page jump
-      var yposButtonDiff = event.target.getBoundingClientRect().top - yposButton;
-      window.scrollTo(window.scrollX, window.scrollY+yposButtonDiff);
+    if (isButtonEvent) {
+        // reset screen to the same position relative to clicked button to prevent page jump
+        var yposButtonDiff = event.target.getBoundingClientRect().top - yposButton;
+        window.scrollTo(window.scrollX, window.scrollY + yposButtonDiff);
 
-      // Store the selection to make it persistent
-      if(window.localStorage){
-          var selectionsJSON = window.localStorage.getItem("tabSelections");
-          if(selectionsJSON){
-            var tabSelections = JSON.parse(selectionsJSON);
-          }else{
-            var tabSelections = {};
-          }
-          tabSelections[tabGroup] = tabId;
-          window.localStorage.setItem("tabSelections", JSON.stringify(tabSelections));
-      }
+        // Store the selection to make it persistent
+        if (window.localStorage) {
+            var selectionsJSON = window.localStorage.getItem("tabSelections");
+            if (selectionsJSON) {
+                var tabSelections = JSON.parse(selectionsJSON);
+            } else {
+                var tabSelections = {};
+            }
+            tabSelections[tabGroup] = tabId;
+            window.localStorage.setItem("tabSelections", JSON.stringify(tabSelections));
+        }
     }
 }
 
 function restoreTabSelections() {
-    if(window.localStorage){
+    if (window.localStorage) {
         var selectionsJSON = window.localStorage.getItem("tabSelections");
-        if(selectionsJSON){
-          var tabSelections = JSON.parse(selectionsJSON);
-        }else{
-          var tabSelections = {};
+        if (selectionsJSON) {
+            var tabSelections = JSON.parse(selectionsJSON);
+        } else {
+            var tabSelections = {};
         }
-        Object.keys(tabSelections).forEach(function(tabGroup) {
-          var tabItem = tabSelections[tabGroup];
-          switchTab(tabGroup, tabItem);
+        Object.keys(tabSelections).forEach(function (tabGroup) {
+            var tabItem = tabSelections[tabGroup];
+            switchTab(tabGroup, tabItem);
         });
     }
 }
 
 // for the window resize
-$(window).resize(function() {
+$(window).resize(function () {
     setMenuHeight();
 });
 
 // debouncing function from John Hann
 // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-(function($, sr) {
+(function ($, sr) {
 
-    var debounce = function(func, threshold, execAsap) {
+    var debounce = function (func, threshold, execAsap) {
         var timeout;
 
         return function debounced() {
@@ -128,17 +128,17 @@ $(window).resize(function() {
         };
     }
     // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+    jQuery.fn[sr] = function (fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
 })(jQuery, 'smartresize');
 
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     restoreTabSelections();
 
-    jQuery('#sidebar .category-icon').on('click', function() {
-        $( this ).toggleClass("fa-angle-down fa-angle-right") ;
-        $( this ).parent().parent().children('ul').toggle() ;
+    jQuery('#sidebar .category-icon').on('click', function () {
+        $(this).toggleClass("fa-angle-down fa-angle-right");
+        $(this).parent().parent().children('ul').toggle();
         return false;
     });
 
@@ -146,25 +146,25 @@ jQuery(document).ready(function() {
     $('#sidebar .highlightable').perfectScrollbar();
     setMenuHeight();
 
-    jQuery('#overlay').on('click', function() {
+    jQuery('#overlay').on('click', function () {
         jQuery(document.body).toggleClass('sidebar-hidden');
         sidebarStatus = (jQuery(document.body).hasClass('sidebar-hidden') ? 'closed' : 'open');
 
         return false;
     });
 
-    jQuery('[data-sidebar-toggle]').on('click', function() {
+    jQuery('[data-sidebar-toggle]').on('click', function () {
         jQuery(document.body).toggleClass('sidebar-hidden');
         sidebarStatus = (jQuery(document.body).hasClass('sidebar-hidden') ? 'closed' : 'open');
 
         return false;
     });
-    jQuery('[data-clear-history-toggle]').on('click', function() {
+    jQuery('[data-clear-history-toggle]').on('click', function () {
         sessionStorage.clear();
         location.reload();
         return false;
     });
-    jQuery('[data-search-toggle]').on('click', function() {
+    jQuery('[data-search-toggle]').on('click', function () {
         if (sidebarStatus == 'closed') {
             jQuery('[data-sidebar-toggle]').trigger('click');
             jQuery(document.body).removeClass('searchbox-hidden');
@@ -180,7 +180,7 @@ jQuery(document).ready(function() {
     });
 
     var ajax;
-    jQuery('[data-search-input]').on('input', function() {
+    jQuery('[data-search-input]').on('input', function () {
         var input = jQuery(this),
             value = input.val(),
             items = jQuery('[data-nav-id]');
@@ -198,15 +198,15 @@ jQuery(document).ready(function() {
 
         if (ajax && ajax.abort) ajax.abort();
 
-        jQuery('[data-search-clear]').on('click', function() {
+        jQuery('[data-search-clear]').on('click', function () {
             jQuery('[data-search-input]').val('').trigger('input');
             sessionStorage.removeItem('search-input');
             $(".highlightable").unhighlight({ element: 'mark' })
         });
     });
 
-    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
-        return function( elem ) {
+    $.expr[":"].contains = $.expr.createPseudo(function (arg) {
+        return function (elem) {
             return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
         };
     });
@@ -220,7 +220,7 @@ jQuery(document).ready(function() {
         if (searchedElem) {
             searchedElem.scrollIntoView(true);
             var scrolledY = window.scrollY;
-            if(scrolledY){
+            if (scrolledY) {
                 window.scroll(0, scrolledY - 125);
             }
         }
@@ -228,30 +228,30 @@ jQuery(document).ready(function() {
 
     // clipboard
     var clipInit = false;
-    $('code').each(function() {
+    $('code').each(function () {
         var code = $(this),
             text = code.text();
 
-        if (text.length > 5) {
+        if (code.parent().prop('tagName') == 'PRE') {
             if (!clipInit) {
                 var text, clip = new ClipboardJS('.copy-to-clipboard', {
-                    text: function(trigger) {
+                    text: function (trigger) {
                         text = $(trigger).prev('code').text();
                         return text.replace(/^\$\s/gm, '');
                     }
                 });
 
                 var inPre;
-                clip.on('success', function(e) {
+                clip.on('success', function (e) {
                     e.clearSelection();
                     inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
                     $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
                 });
 
-                clip.on('error', function(e) {
+                clip.on('error', function (e) {
                     inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
                     $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                    $(document).one('copy', function(){
+                    $(document).one('copy', function () {
                         $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
                     });
                 });
@@ -259,60 +259,60 @@ jQuery(document).ready(function() {
                 clipInit = true;
             }
 
-            code.after('<span class="copy-to-clipboard" title="Copy to clipboard" />');
-            code.next('.copy-to-clipboard').on('mouseleave', function() {
+            code.after('<span class="copy-to-clipboard" title="Copy to clipboard">Copy</span>');
+            code.next('.copy-to-clipboard').on('mouseleave', function () {
                 $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
             });
         }
     });
 
     // allow keyboard control for prev/next links
-    jQuery(function() {
-        jQuery('.nav-prev').click(function(){
+    jQuery(function () {
+        jQuery('.nav-prev').click(function () {
             location.href = jQuery(this).attr('href');
         });
-        jQuery('.nav-next').click(function() {
+        jQuery('.nav-next').click(function () {
             location.href = jQuery(this).attr('href');
         });
     });
 
     jQuery('input, textarea').keydown(function (e) {
-         //  left and right arrow keys
-         if (e.which == '37' || e.which == '39') {
-             e.stopPropagation();
-         }
-     });
+        //  left and right arrow keys
+        if (e.which == '37' || e.which == '39') {
+            e.stopPropagation();
+        }
+    });
 
-    jQuery(document).keydown(function(e) {
-      // prev links - left arrow key
-      if(e.which == '37') {
-        jQuery('.nav.nav-prev').click();
-      }
+    jQuery(document).keydown(function (e) {
+        // prev links - left arrow key
+        if (e.which == '37') {
+            jQuery('.nav.nav-prev').click();
+        }
 
-      // next links - right arrow key
-      if(e.which == '39') {
-        jQuery('.nav.nav-next').click();
-      }
+        // next links - right arrow key
+        if (e.which == '39') {
+            jQuery('.nav.nav-next').click();
+        }
     });
 
     $('#top-bar a:not(:has(img)):not(.btn)').addClass('highlight');
     $('#body-inner a:not(:has(img)):not(.btn):not(a[rel="footnote"])').addClass('highlight');
 
     var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
-    if (!touchsupport){ // browser doesn't support touch
-        $('#toc-menu').hover(function() {
+    if (!touchsupport) { // browser doesn't support touch
+        $('#toc-menu').hover(function () {
             $('.progress').stop(true, false, true).fadeToggle(100);
         });
 
-        $('.progress').hover(function() {
+        $('.progress').hover(function () {
             $('.progress').stop(true, false, true).fadeToggle(100);
         });
     }
-    if (touchsupport){ // browser does support touch
-        $('#toc-menu').click(function() {
+    if (touchsupport) { // browser does support touch
+        $('#toc-menu').click(function () {
             $('.progress').stop(true, false, true).fadeToggle(100);
         });
-        $('.progress').click(function() {
+        $('.progress').click(function () {
             $('.progress').stop(true, false, true).fadeToggle(100);
         });
     }
@@ -402,7 +402,7 @@ jQuery(document).ready(function() {
 
 });
 
-jQuery(window).on('load', function() {
+jQuery(window).on('load', function () {
     // store this page in session
     sessionStorage.setItem(jQuery('body').data('url'), 1);
 
@@ -415,14 +415,14 @@ jQuery(window).on('load', function() {
     $(".highlightable").highlight(sessionStorage.getItem('search-value'), { element: 'mark' });
 });
 
-$(function() {
+$(function () {
     $('a[rel="lightbox"]').featherlight({
         root: 'section#body'
     });
 });
 
 jQuery.extend({
-    highlight: function(node, re, nodeName, className) {
+    highlight: function (node, re, nodeName, className) {
         if (node.nodeType === 3) {
             var match = node.data.match(re);
             if (match) {
@@ -446,21 +446,21 @@ jQuery.extend({
     }
 });
 
-jQuery.fn.unhighlight = function(options) {
+jQuery.fn.unhighlight = function (options) {
     var settings = {
         className: 'highlight',
         element: 'span'
     };
     jQuery.extend(settings, options);
 
-    return this.find(settings.element + "." + settings.className).each(function() {
+    return this.find(settings.element + "." + settings.className).each(function () {
         var parent = this.parentNode;
         parent.replaceChild(this.firstChild, this);
         parent.normalize();
     }).end();
 };
 
-jQuery.fn.highlight = function(words, options) {
+jQuery.fn.highlight = function (words, options) {
     var settings = {
         className: 'highlight',
         element: 'span',
@@ -474,10 +474,10 @@ jQuery.fn.highlight = function(words, options) {
     if (words.constructor === String) {
         words = [words];
     }
-    words = jQuery.grep(words, function(word, i) {
+    words = jQuery.grep(words, function (word, i) {
         return word != '';
     });
-    words = jQuery.map(words, function(word, i) {
+    words = jQuery.map(words, function (word, i) {
         return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
     });
     if (words.length == 0) { return this; }
@@ -490,7 +490,7 @@ jQuery.fn.highlight = function(words, options) {
     }
     var re = new RegExp(pattern, flag);
 
-    return this.each(function() {
+    return this.each(function () {
         jQuery.highlight(this, re, settings.element, settings.className);
     });
 };
