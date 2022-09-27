a482cff1f05a83d3ef76f9ff5f37412cd39984cf
Jack Keene <jack.keene@snowplowanalytics.com>
2022-09-27 16:51:25 +0100

    Add netlify deployment

---
+++ .github/scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Bash script to deploy hugo website to github"
+echo "Setting Local Variables..."
+export TITLE="Advanced Analytics for Web"
+export DESCRIPTION="Tutorial for setting up advanced analytics on a website"
+export BASE_URL="https://docs.snowplow.io/advanced-analytics-web-accelerator/"
+export AUTHOR="Agnes Kiss & Jack Keene" 
+export LANDING_PAGE_URL="/advanced-analytics-web-accelerator/en/introduction"
+
+echo "Importing themes..."
+mkdir main
+mkdir main/themes
+cp -R content main/
+cp -R accelerator-web-ui-template/themes/hugo-theme-learn main/themes/
+cp -R accelerator-web-ui-template/layouts main/
+cp -R accelerator-web-ui-template/static main/
+cp -R accelerator-web-ui-template/config.toml main/
+cd main
+sed -i '' "s|ACCELERATOR_TITLE|$TITLE|g" config.toml
+sed -i '' "s|ACCELERATOR_DESCRIPTION|$DESCRIPTION|g" config.toml
+sed -i '' "s|ACCELERATOR_BASE_URL|$BASE_URL|g" config.toml
+sed -i '' "s|ACCELERATOR_AUTHOR|$AUTHOR|g" config.toml
+sed -i '' "s|ACCELERATOR_LANDING_PAGE_URL|$LANDING_PAGE_URL|g" config.toml
+echo "Creating Hugo site..."
+hugo
+cp -R public ../
+cd ..
+rm -r main
+
+
