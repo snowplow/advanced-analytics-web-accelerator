272a38f5d54c83a03e65d65e95a01e2ec3863941
Jack Keene <jack.keene@snowplowanalytics.com>
2022-09-28 15:14:58 +0100

    Edit sed commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "Bash script to deploy hugo website to github"
 echo "Setting Local Variables..."
-export TITLE="Advanced Analytics for Web"
-export DESCRIPTION="Tutorial for setting up advanced analytics on a website"
-export BASE_URL="https://docs.snowplow.io/advanced-analytics-web-accelerator/"
-export AUTHOR="Agnes Kiss and Jack Keene" 
-export LANDING_PAGE_URL="/advanced-analytics-web-accelerator/en/introduction"
-
+TITLE="Advanced Analytics for Web"
+DESCRIPTION="Tutorial for setting up advanced analytics on a website"
+BASE_URL="https://docs.snowplow.io/advanced-analytics-web-accelerator/"
+AUTHOR="Agnes Kiss and Jack Keene" 
+LANDING_PAGE_URL="/advanced-analytics-web-accelerator/en/introduction"
 echo "Importing themes..."
 mkdir main
 mkdir main/themes
@@ -15,14 +14,15 @@ cp -R accelerator-web-ui-template/themes/hugo-theme-learn main/themes/
 cp -R accelerator-web-ui-template/layouts main/
 cp -R accelerator-web-ui-template/static main/
 cp -R accelerator-web-ui-template/config.toml main/
+cp -R accelerator-web-ui-template/netlify.toml main/
 cd main
 git submodule update --init --recursive
-sed  "s|ACCELERATOR_TITLE|$TITLE|g" config.toml | sponge config.toml
-sed  "s|ACCELERATOR_DESCRIPTION|$DESCRIPTION|g" config.toml | sponge config.toml
-sed "s|ACCELERATOR_BASE_URL|$BASE_URL|g" config.toml | sponge config.toml
-sed "s|ACCELERATOR_AUTHOR|$AUTHOR|g" config.toml | sponge config.toml
-sed  "s|ACCELERATOR_LANDING_PAGE_URL|$LANDING_PAGE_URL|g" config.toml | sponge config.toml
 echo "Creating Hugo site..."
+sed -i -e "s|ACCELERATOR_TITLE|$TITLE|g" config.toml
+sed -i -e "s|ACCELERATOR_DESCRIPTION|$DESCRIPTION|g" config.toml
+sed -i -e "s|ACCELERATOR_BASE_URL|$BASE_URL|g" config.toml
+sed -i -e "s|ACCELERATOR_AUTHOR|$AUTHOR|g" config.toml
+sed -i -e "s|ACCELERATOR_LANDING_PAGE_URL|$LANDING_PAGE_URL|g" config.toml
 hugo
 cp -R public ../
 cd ..
